bc9abe46a32cf989f0271f3b26ce3fe6622be692
AHETESHAMS <aheteshams007@gmail.com>
2020-01-14 14:29:57 +0530

    Added:-Wage calculated till condition reachd to 20 days and 100 hours

--- Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
+++ Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
 echo "Welcome To Employee Wage...."
-i=0
 dailyWage=0
 workingDays=0
-while [[ $i -le 20 ]]
+workHours=0
+while [[ $workingDays -lt 20 && $workHours -lt 100 ]]
 do
 	randomCheck=$(( RANDOM%3 ))
 case $randomCheck  in
 	1)
 		echo "Employee is Present For Full Time"
 		(( workingDays++ ))
+		workHours=$(( workHours+8 ))
 		totaldDailyWage=$(( totaldDailyWage+20*8 ))
 		;;
 	0)
@@ -18,11 +19,13 @@ case $randomCheck  in
 	2)
       echo "Employee Present for Part Time"
       (( workingDays++ ))
+		 workHours=$(( workHours+4 ))
       totaldDailyWage=$(( totaldDailyWage+20*4 ))
 		;;
 esac
 (( i++ ))
 done
 echo "Total Working Days are:-"$workingDays
+echo "Total Working Hours are :-"$workHours
 echo "Total Wage is:-"$totaldDailyWage
 
