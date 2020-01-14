a812c6a93a7ee8843a2cac1fa5d47084d330bff4
AHETESHAMS <aheteshams007@gmail.com>
2020-01-14 12:39:18 +0530

    Added: Solved with Case

--- Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
+++ Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
@@ -6,20 +6,22 @@ workingDays=0
 while [[ $i -le 10 ]]
 do
 	randomCheck=$(( RANDOM%3 ))
-	if [ $randomCheck -eq 1 ]
-	then
-		echo "Employee is Present For Full Time"
-		(( workingDays++ ))
-		totaldDailyWage=$(( totaldDailyWage+20*8 ))
-	elif [ $randomCheck -eq 0 ]
-	then
-		echo "Employee is Absent"
-	else
-      echo "Employee Present for Part Time"
-       (( workingDays++ ))
-       totaldDailyWage=$(( totaldDailyWage+20*4 ))
-	fi
-	(( i++ ))
+case $randomCheck  in
+							1)
+								echo "Employee is Present For Full Time"
+								(( workingDays++ ))
+								totaldDailyWage=$(( totaldDailyWage+20*8 ))
+								;;
+							0)
+								echo "Employee is Absent"
+								;;
+							2)
+      						echo "Employee Present for Part Time"
+       						(( workingDays++ ))
+       						totaldDailyWage=$(( totaldDailyWage+20*4 ))
+								;;
+esac
+(( i++ ))
 done
 echo "Total Working Days are:-"$workingDays
 echo "Total Wage is:-"$totaldDailyWage
