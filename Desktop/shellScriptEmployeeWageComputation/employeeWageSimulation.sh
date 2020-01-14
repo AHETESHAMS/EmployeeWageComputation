e0a203df5245246a87d078b2a89dbf296dfcfd92
AHETESHAMS <aheteshams007@gmail.com>
2020-01-14 11:34:18 +0530

    Added: Employee is Present or Absent

--- Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
+++ Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "Welcome To Employee Wage...."
+randomCheck=$(( RANDOM%2 ))
+if [[ $randomCheck -ge 1 ]]
+then
+	echo "Employee is Present"
+elif [[ $randomCheck -eq 0 ]]
+then
+	echo "Employee is Absent"
+fi
+
