ada8d0f277db6815c7ef3c93ede7d1948894a38e
AHETESHAMS <aheteshams007@gmail.com>
2020-01-14 11:58:15 +0530

    Added: Number of Days working and Total Daily Wage

--- Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
+++ Desktop/shellScriptEmployeeWageComputation/employeeWageSimulation.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -x
 echo "Welcome To Employee Wage...."
-randomCheck=$(( RANDOM%2 ))
-if [[ $randomCheck -ge 1 ]]
-then
-	echo "Employee is Present"
-elif [[ $randomCheck -eq 0 ]]
-then
-	echo "Employee is Absent"
-fi
+i=0
+dailyWage=0
+workingDays=0
+while [[ $i -le 10 ]]
+do
+	randomCheck=$(( RANDOM%2 ))
+	if [ $randomCheck -ge 1 ]
+	then
+		echo "Employee is Present"
+		(( workingDays++ ))
+		totaldDailyWage=$(( totaldDailyWage+20*8 ))
+	elif [ $randomCheck -eq 0 ]
+	then
+		echo "Employee is Absent"
+	fi
+	(( i++ ))
+done
+echo "Total Working Days are:-"$workingDays
+echo "Total Wage is:-"$totaldDailyWage
 
